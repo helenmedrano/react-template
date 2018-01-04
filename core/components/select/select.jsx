3e59880671eff352f735715f3e8095bec9e50c34
Brian Lee <briandl92391@gmail.com>
2018-01-04 11:21:55 -0800

    Switches setState to callback in Select click handler

--- core/components/select/select.jsx
+++ core/components/select/select.jsx
@@ -62,7 +62,9 @@ class Select extends React.Component {
   }
 
   handleClick = event => {
-    this.setState({ open: !this.state.open, anchor: event.currentTarget })
+    this.setState(prevState => {
+      return { open: !prevState.open, anchor: event.currentTarget }
+    })
   }
 
   handleOptionSelect = value => {
