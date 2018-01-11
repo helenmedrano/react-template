56aa295381fe0a330059a4bce4619cb3c349cf2b
Brian Lee <briandl92391@gmail.com>
2018-01-11 10:42:15 -0800

    Updates message import to use default import (#32)

--- examples/firebase_authentication/pages/demo/demo.js
+++ examples/firebase_authentication/pages/demo/demo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { Message } from 'core/components/message'
+import Message from 'core/components/message'
 import AuthContainer from 'firebase_authentication/components/auth_container'
 
 const StyledRootContainer = styled.div`
